200f5fccc4799e7d69134bd67d74f473db4c4acb
asistentedebeneficios-svg <asistentedebeneficios@gmail.com>
2026-03-11 15:25:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5952,7 +5952,7 @@ const ClientReviewScreen = ({ leadId, db }) => {
                     {/* INSTRUCCIÓN CLARA PARA ADULTOS MAYORES */}
                     <div className="bg-amber-50 border border-amber-200 px-4 py-2 rounded-full mb-5 shadow-sm animate-pulse">
                         <p className="text-xs sm:text-sm font-extrabold text-amber-700 tracking-wide flex items-center gap-2">
-                            👇 Por favor, elija entre 1 a 5 estrellas.
+                            👇 Elija entre 1 a 5 estrellas.
                         </p>
                     </div>
 
